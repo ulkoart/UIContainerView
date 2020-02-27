e49d2e7e6c9f93a6a37e61ad81fecf37a8280485
Artem Ulko <ulkoart@gmail.com>
2020-02-27 14:39:14 +0300

    init+

---
+++ UIContainerView/DetailViewControler.swift
@@ -0,0 +1,107 @@
+//
+//  DetailViewControler.swift
+//  UIContainerView
+//
+//  Created by user on 27/02/2020.
+//  Copyright © 2020 Artem Ulko. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewControler: UITableViewController {
+    let imageNameArray = ["Alberto Ruiz - 7 Elements (Original Mix)",
+                          "Dave Wincent - Red Eye (Original Mix)",
+                          "E-Spectro - End Station (Original Mix)",
+                          "Edna Ann - Phasma (Konstantin Yoodza Remix)",
+                          "Ilija Djokovic - Delusion (Original Mix)",
+                          "John Baptiste - Mycelium (Original Mix)",
+                          "Lane 8 - Fingerprint (Original Mix)",
+                          "Mac Vaughn - Pink Is My Favorite Color (Alex Stein Remix)",
+                          "Metodi Hristov, Gallya - Badmash (Original Mix)",
+                          "Veerus, Maxie Devine - Nightmare (Original Mix)"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return imageNameArray.count
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Title", for: indexPath)
+        
+        cell.imageView?.image = UIImage(named:imageNameArray[indexPath.row])
+        cell.textLabel?.text = imageNameArray[indexPath.row]
+        cell.textLabel?.numberOfLines = 0
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let viewControler = parent as? ViewController {
+            let trackTitle = imageNameArray[indexPath.row]
+            viewControler.titlelabel.text = trackTitle
+            viewControler.imageCover.image = UIImage(named: trackTitle)
+        }
+    }
+    
+    
+    /*
+     // Override to support conditional editing of the table view.
+     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the specified item to be editable.
+     return true
+     }
+     */
+    
+    /*
+     // Override to support editing the table view.
+     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+     if editingStyle == .delete {
+     // Delete the row from the data source
+     tableView.deleteRows(at: [indexPath], with: .fade)
+     } else if editingStyle == .insert {
+     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+     }
+     }
+     */
+    
+    /*
+     // Override to support rearranging the table view.
+     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+     }
+     */
+    
+    /*
+     // Override to support conditional rearranging of the table view.
+     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the item to be re-orderable.
+     return true
+     }
+     */
+    
+    
+    // MARK: - Navigation
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        if segue.identifier == "ShowDetail" {
+//            if let indexPath = self.tableView.indexPathForSelectedRow {
+//                let detailVC = segue.destination as! DetailVC
+//                detailVC.tracktitle = imageNameArray[indexPath.row]
+//            }
+//        }
+//    }
+    
+    
+}
--- UIContainerView/ViewController.swift
+++ UIContainerView/ViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var imageCover: UIImageView!
+    @IBOutlet weak var titlelabel: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
